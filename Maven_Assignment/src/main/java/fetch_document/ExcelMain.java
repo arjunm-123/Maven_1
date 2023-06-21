29539a845f0c9c8cb77caa70ba69c408c8694824
HP <HP@DESKTOP-RJLER7D.bbrouter>
2023-06-21 22:38:38 +0530

    Commit

---
+++ Maven_Assignment/src/main/java/fetch_document/ExcelMain.java
@@ -0,0 +1,15 @@
+package fetch_document;
+
+import java.io.IOException;
+
+
+public class ExcelMain {
+
+	public static void main(String[] args) throws IOException 
+	{
+		// TODO Auto-generated method stub
+		System.out.println(ExcelCode.readStringData(0, 1));
+		System.out.println(ExcelCode.readIntegerData(0,0));
+	}
+
+}
